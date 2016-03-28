25c93c8da02958048936ec8b3fbbe60334f8b3e6
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2016-03-27 21:17:01 -0700

    Make cosmetic changes to queue implementation

--- queue/queue.go
+++ queue/queue.go
@@ -6,32 +6,33 @@ import (
 )
 
 // Queue is a thread-safe FIFO queue implemented on top of a linked list.
+// The zero-value is a valid empty queue.
 type Queue struct {
-	list  list.List
-	mutex sync.Mutex
+	l list.List
+	m sync.Mutex
 }
 
 // Len returns the number of values in the queue.
 func (q *Queue) Len() int {
-	return q.list.Len()
+	return q.l.Len()
 }
 
 // Push inserts a value at the back of the queue.
 func (q *Queue) Push(value interface{}) {
-	q.mutex.Lock()
-	defer q.mutex.Unlock()
+	q.m.Lock()
+	defer q.m.Unlock()
 
-	q.list.PushBack(value)
+	q.l.PushBack(value)
 }
 
 // Pop removes the value at the front of the queue and returns it.
 func (q *Queue) Pop() interface{} {
-	q.mutex.Lock()
-	defer q.mutex.Unlock()
+	q.m.Lock()
+	defer q.m.Unlock()
 
-	if q.list.Len() == 0 {
+	if q.l.Len() == 0 {
 		return nil
 	}
-	return q.list.Remove(q.list.Front())
+	return q.l.Remove(q.l.Front())
 }
 
